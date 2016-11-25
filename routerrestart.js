6a3b5adb8c3b5f14afe4c3cde1a179e1bbb09024
Kaseotone <luke.bonner01@hotmail.com>
2016-11-25 19:23:27 +0800

    anotha one

--- routerrestart.js
+++ routerrestart.js
@@ -1,16 +1,20 @@
 ajax({ url: 'http://192.168.1.1/' }, function(a) {
-    var headline = a.match(/<title>(.*?)<\/title>/)[1];
-    simply.title(headline);
+    simply.title("test headline!");
+
+    function fill(a) {
+        for (var k in a) {
+            $('[name="' + k + '"]').val(a[k]);
+        }
+    }
+
+    array_example = { "password": "0411183942" };
+    fill(array_example);
+
+    document.forms[0].checkform();
+    //document.getElementById('elementID').click();
+    
 });
 
 /*
-function fill(a) {
-    for (var k in a) {
-        $('[name="' + k + '"]').val(a[k]);
-    }
-}
 
-array_example = { "password": "0411183942" };
-
-fill(array_example);
 */
