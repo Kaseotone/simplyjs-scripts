097d8fc17ec5c8cd254bb7fdf8df5ed4b994c382
Kaseotone <luke.bonner01@hotmail.com>
2016-11-25 20:31:43 +0800

    ok ig????????

--- routerrestart.js
+++ routerrestart.js
@@ -1,10 +1,9 @@
-ajax({ url: 'http://192.168.1.1/' }, function (a) {
-    a.getElementById('password').value = "0411183942";
-    a.forms[0].checkform();
-    //document.getElementById('elementID').click();
+document.getElementById('password').value = "0411183942";
+document.forms[0].checkform();
+//document.getElementById('elementID').click();
+
+/*ajax({ url: 'http://192.168.1.1/' }, function (a) {
+
 
 });
-
-/*
-
 */
