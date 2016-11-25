b671ba773ab2288517b3f7e17e8a8b313efc135e
Kaseotone <luke.bonner01@hotmail.com>
2016-11-25 19:42:07 +0800

    big ol rewrite bc i'm an idiot

--- routerrestart.js
+++ routerrestart.js
@@ -1,18 +1,9 @@
 
 
 ajax({ url: 'http://192.168.1.1/' }, function (a) {
-    simply.title("test headline!");
+    a.getElementById('password').value = "0411183942"
 
-    function fill(a) {
-        for (var k in a) {
-            jQuery('[name="' + k + '"]').val(a[k]);
-        }
-    }
-
-    array_example = { "password": "0411183942" };
-    fill(array_example);
-
-    document.forms[0].checkform();
+    a.forms[0].checkform();
     //document.getElementById('elementID').click();
     
 });
